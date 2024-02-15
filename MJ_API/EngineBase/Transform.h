c873f9df12bd3da95f4949e4225b0bca1bda7bd9
KMJ 김명지 <https://github.com/gee-kim/MJ.git>
2024-02-15 14:59:38 +0900

    240215 충돌 내용 추가

--- MJ_API/EngineBase/Transform.h
+++ MJ_API/EngineBase/Transform.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "EngineMath.h"
 
-enum ECollisionType
+enum class ECollisionType
 {
 	Point,
 	CirCle,
 	Rect,
+	Max,
 };
 
 // 설명 :
@@ -15,7 +16,7 @@ class FTransform
 	friend CollisionFunctionInit;
 
 private:
-	static bool (*CollisionFunction[static_cast<int>(Rect)][static_cast<int>(Rect)])(const FTransform& _Left, const FTransform& _Right);
+	static bool (*CollisionFunction[static_cast<int>(ECollisionType::Max)][static_cast<int>(ECollisionType::Max)])(const FTransform& _Left, const FTransform& _Right);
 
 public:
 	// constrcuter destructer
@@ -36,7 +37,17 @@ public:
 	// bool CircleToPoint(const FTransform& _Left, const FTransform& _Right);
 
 	static bool CircleToCircle(const FTransform& _Left, const FTransform& _Right);
+	static bool CircleToRect(const FTransform& _Left, const FTransform& _Right);
+	static bool CircleToPoint(const FTransform& _Left, const FTransform& _Right);
 
+	static bool RectToRect(const FTransform& _Left, const FTransform& _Right);
+	static bool RectToCircle(const FTransform& _Left, const FTransform& _Right);
+
+
+	static bool RectToPoint(const FTransform& _Left, const FTransform& _Right);
+
+	static bool PointToRect(const FTransform& _Left, const FTransform& _Right);
+	static bool PointToCircle(const FTransform& _Left, const FTransform& _Right);
 public:
 	void SetScale(FVector _Value)
 	{
@@ -57,6 +68,26 @@ public:
 	FVector GetPosition() const
 	{
 		return Position;
+	}
+
+	FVector LeftTop() const
+	{
+		return { Left(), Top() };
+	}
+
+	FVector RightTop() const
+	{
+		return { Right(), Top() };
+	}
+
+	FVector LeftBottom() const
+	{
+		return { Left(), Bottom() };
+	}
+
+	FVector RightBottom() const
+	{
+		return { Right(), Bottom() };
 	}
 
 	float Left() const
@@ -93,7 +124,18 @@ public:
 		return std::lround(Bottom());
 	}
 
-	// 충돌을 이녀석이 가질까?
+	void SetRadius(float _Radius) 
+	{
+		Scale = float4::Zero;
+		Scale.X = _Radius * 2.0f;
+	}
+
+	float GetRadius() const
+	{
+		return Scale.hX();
+	}
+
+	// 충돌을 왜 Transform이 가질까?
 	// 위치와 크기를 가지고 있기 때문에.
 	bool Collision(ECollisionType _ThisType, ECollisionType _OtherType, const FTransform& _Other);
 
